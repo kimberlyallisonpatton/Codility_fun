1e0036a8fee8167ac399dd4dcd1e21ae43d72bc6
kapatton90 <kapatton19@gmail.com>
2016-10-26 16:21:08 -0500

    Complete solution for Odd Occurences in Array. Still working on improvig the performance to make the solution faster.

--- Odd_Occurrences_In_Array/solution.rb
+++ Odd_Occurrences_In_Array/solution.rb
@@ -1,1 +1,13 @@
-solution.rb
+def solution(a)
+	array_to_string = a.join
+	i = 0 
+	until i == a.length 
+		x = a[i].to_s
+		if array_to_string.scan(Regexp.escape(x)).count.odd?
+			answer = x.to_i
+			break
+		end
+		i += 1 
+	end
+	return answer
+end
