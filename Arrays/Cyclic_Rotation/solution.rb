aab54ea64ad70c005f2e554d800586c97f885bf6
kapatton90 <kapatton19@gmail.com>
2017-01-07 09:55:00 -0600

    Very simple solution. Good reminder to always read the docs thoroughly.

---
+++ Arrays/Cyclic_Rotation/solution.rb
@@ -0,0 +1,20 @@
+# I feel so dumb! I just needed to read the docs more thoroughly.
+def solution(a, k)
+  a.rotate!(-k)
+end
+
+# 25% so bad, only works for bare minimum
+def solution(a, k)
+  a.rotate!(k - 1)
+end
+
+
+# 87% accuracy
+def solution(a, k)
+	k.times do
+		popped_array = []
+		popped_array << a.pop
+		a = popped_array.concat(a) 
+	end
+	return a
+end
